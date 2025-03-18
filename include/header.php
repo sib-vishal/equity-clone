0dee8ca943a24d20891e030b5e8bce8f121c077d
sib-vishal <vishal.maurya@sibinfo.tech>
2025-03-18 15:47:32 +0530

    commit

--- include/header.php
+++ include/header.php
@@ -12,24 +12,29 @@
 				<div class="menu-links">
 
 					<ul>
-					<li><a href="inedex.php">Home</a></li>
-							<li><a href="about-us.php">About Us</a></li>
-							<li><a href="services.php">Services</a></li>
-							<li><a href="events.php">Events and Conferences</a></li>
-							<li><a href="contact-us.php"> Contact Us</a></li>
+						<li><a class="<?php echo ($page == 'home') ? 'active' : ''; ?>" href="index.php">Home</a></li>
+						<li><a class="<?php echo ($page == 'about-us') ? 'active' : ''; ?>" href="about-us.php">About
+								Us</a></li>
+						<li><a class="<?php echo ($page == 'services') ? 'active' : ''; ?>"
+								href="services.php">Services</a></li>
+						<li><a class="<?php echo ($page == 'events') ? 'active' : ''; ?>" href="events.php">Events and
+								Conferences</a></li>
+						<li><a class="<?php echo ($page == 'contact-us') ? 'active' : ''; ?>"
+								href="contact-us.php">Contact Us</a></li>
 					</ul>
+
 
 
 
 				</div>
 				<div class="header_btn">
-					<div class="btn_icon"><svg stroke="currentColor"
-								fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round"
-								stroke-linejoin="round" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg">
-								<path
-									d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
-								</path>
-							</svg></div>
+					<div class="btn_icon"><svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24"
+							stroke-linecap="round" stroke-linejoin="round" height="1em" width="1em"
+							xmlns="http://www.w3.org/2000/svg">
+							<path
+								d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+							</path>
+						</svg></div>
 					<div class="btn_content">
 						<p>Need help? Talk to an expert</p>
 						<h5><a href="tel:12463330079">+1- ( 246 ) 333 - 0079</a></h5>
